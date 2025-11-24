c61b560583eb861f185d27a2ad136b8f51861a30
JonasPfalzgraf <20913954+JosunLP@users.noreply.github.com>
2025-11-24 01:30:59 +0100

    feat: update package.json scripts and dependencies; add vsce for packaging
    chore: add new VSIX file for pulse-api-client version 2.0.0
    
    fix: update message constants for improved clarity and consistency

--- src/constants/message.ts
+++ src/constants/message.ts
@@ -3,10 +3,10 @@ const MESSAGE = {
   YES: "Yes",
   ERROR: "Error",
   NOT_FOUND: "Not Found",
-  WELCOME_MESSAGE: "Welcome to REST API Tester 😊",
-  WARNING_MESSAGE: "You must enter a request URL to receive a response 😔",
+  WELCOME_MESSAGE: "Welcome to Pulse API Tester",
+  WARNING_MESSAGE: "You must enter a request URL to receive a response",
   COPY_SUCCESFUL_MESSAGE: "Copied to Clipboard ✅",
-  DELETE_REMINDER: "Are you sure you want delete all collection??",
+  DELETE_REMINDER: "Are you sure you want delete all collections?",
 };
 
 export default MESSAGE;
